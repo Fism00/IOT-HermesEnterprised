5593f7001753b73516cbad2f0b1228adbfb94c05
Mordecai <you@example.com>
2025-10-06 10:06:42 -0300

    implementada conexão com wifi

--- S1/S1.ino
+++ S1/S1.ino
@@ -1,6 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
 
+const String ssid = "FIESC_IOT_EDU";
+const String pass = "8120gv08";
+
+void setup() {
+  
+Serial.begin(115200);
+Serial.println("Conectando ao WiFi");
+WiFi.begin(ssid, pass);
+while(WiFi.status() != WL_CONNECTED) {
+  Serial.print(".");
+  delay(200);
+}
+
+Serial.println("\nConectado com sucesso!");
 }
 
 void loop() {
