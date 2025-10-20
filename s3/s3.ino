42959959ef7c4dea0b3a53401e8dc698150b7030
kaua <kaua_r_ribeiro@estudante.seisenai.org.br>
2025-10-20 10:06:31 -0300

    conexao broker

--- s3/s3.ino
+++ s3/s3.ino
@@ -1,9 +1,46 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+WifiClient client;
+PubSubClient mqtt(client);
+
+const String SSID = "FIESC_IOT_EDU";
+const String PASS =  "8120gv08";
+
+const String url   = "test.mosquitto.org";
+const int port     = 1883;
+const String USR   = "";
+const String Pass  = "";
+const String Topic = "DSM1";
+
+
 void setup() {
-  // put your setup code here, to run once:
+ Serial.begin(115200);
+ Serial.println("Conectando ao Wifi");
+ WiFi.begin(SSID, PASS);
+ 
+ while (WiFi.status() != WL_CONNECTED) {
+  Serial.print('.');
+  delay(200);
+ }
+ Serial.println("\nConectado com sucesso!");
 
+ Serial.println("Conectando ao Broker");
+ mqtt.setServer(URL.c_str(),PORT);
+ while(!,qtt.connnected()){
+  String ID = "S3_";
+  ID += String(random(0xffff), HEX); //cria a parte aleat.
+  mqtt.connect(ID.c+str(),USR.c+str(),PASS.c_str());
+  Serial.print('.');
+  delay(200)
+ }
+ Serial.println("\nConectado com sucesso ao broker !");
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
+  String mensagem = "Kaua: ";
+  mensagem += "nao escondi um corpo nas moitas do parque são gonçalo na quinta feira";
 
+  mqtt.loop();
+  delay(1000);
 }
