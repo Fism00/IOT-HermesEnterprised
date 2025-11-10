b4f0393285fca5b54c26ba5ef034425dd9df35c0
kaua <kaua_r_ribeiro@estudante.seisenai.org.br>
2025-11-10 10:02:28 -0300

    sensor da s3

--- s3/s3.ino
+++ s3/s3.ino
@@ -1,46 +1,53 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-WifiClient client;
+WiFiClient client;
 PubSubClient mqtt(client);
 
 const String SSID = "FIESC_IOT_EDU";
 const String PASS =  "8120gv08";
 
-const String url   = "test.mosquitto.org";
-const int port     = 1883;
+const String url   = "d1afbd3a85c7409fa6447c6f1f6ea1ae.s1.eu.hivemq.cloud";
+const int port     = 8883;
 const String USR   = "";
 const String Pass  = "";
 const String Topic = "DSM1";
-
+const String broker_PASS = "";
+const String Mytopic = "Kaus";
+const String OtherTopic = "Klahold";
+const byte TRIGGER_PIN = 5;
+const byte ECHO_PIN = 18;
 
 void setup() {
- Serial.begin(115200);
- Serial.println("Conectando ao Wifi");
- WiFi.begin(SSID, PASS);
- 
- while (WiFi.status() != WL_CONNECTED) {
-  Serial.print('.');
-  delay(200);
- }
- Serial.println("\nConectado com sucesso!");
-
- Serial.println("Conectando ao Broker");
- mqtt.setServer(URL.c_str(),PORT);
- while(!,qtt.connnected()){
-  String ID = "S3_";
-  ID += String(random(0xffff), HEX); //cria a parte aleat.
-  mqtt.connect(ID.c+str(),USR.c+str(),PASS.c_str());
-  Serial.print('.');
-  delay(200)
- }
- Serial.println("\nConectado com sucesso ao broker !");
+  Serial.begin(115200);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 }
- 
+
+long lerDistancia() {
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  
+  long duracao = pulseIn(ECHO_PIN, HIGH);
+  long distancia = duracao * 349.24 / 2 / 10000;
+  
+  return distancia;
+}
+
 void loop() {
-  String mensagem = "Kaua: ";
-  mensagem += "nao escondi um corpo nas moitas do parque são gonçalo na quinta feira";
-
-  mqtt.loop();
-  delay(1000);
+  long distancia = lerDistancia();
+  
+  Serial.print("Distância: ");
+  Serial.print(distancia);
+  Serial.println(" cm");
+  
+  if (distancia < 10) {
+    Serial.println("Objeto próximo!");
+  }
+  
+  delay(500);
 }
+
