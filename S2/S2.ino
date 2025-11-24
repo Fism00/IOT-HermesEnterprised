b306c8b22d5c6cc442a1906cec15d0afcbd6195f
Mordecai <you@example.com>
2025-11-24 10:03:30 -0300

    S2 completa

--- S2/S2.ino
+++ S2/S2.ino
@@ -10,19 +10,52 @@ const byte TRIGGER_ULTRA_1 = 25;
 const byte ECHO_ULTRA_1 = 33;
 const byte TRIGGER_ULTRA_2 = 27;
 const byte ECHO_ULTRA_2 = 34;
+const byte PINO_LED = 19;
 
 //constantes p/broker
 const String URL   = "d1afbd3a85c7409fa6447c6f1f6ea1ae.s1.eu.hivemq.cloud";
 const int PORT     = 8883;
 const String USR   = "Placa_s2";
 const String PASS  = "Placa_s2";
-const String topic = "DSM1";
+
+//topicos
+const String topicPresença1 = "IOT-HermesEnterprise/S2/presenca1";
+const String topicPresença2 = "IOT-HermesEnterprise/S2/presenca2";
+const String topicIlum = "IOT-HermesEnterprise/S1/Ilum";
 
 //constantes p/wifi
 const String ssid = "FIESC_IOT_EDU";
 const String pass = "8120gv08";
 
+// callback para acender o led
+void callback(char* topic, byte* payload, unsigned int length) {
+  String mensagem;
+
+  // Transforma o payload recebido em texto
+  for (unsigned int i = 0; i < length; i++)
+    mensagem += (char)payload[i];
+
+  Serial.print("Recebido em [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  Serial.println(mensagem);
+
+  // Se a mensagem veio do tópico de iluminação
+  if (String(topic) == topicIlum) {
+
+    if (mensagem == "escuro")
+      digitalWrite(PINO_LED, HIGH);  // Liga LED
+
+    else if (mensagem == "claro")
+      digitalWrite(PINO_LED, LOW);   // Desliga LED
+  }}
+
+  
+
 void setup() {
+
+  pinMode(PINO_LED, OUTPUT);
+  digitalWrite(PINO_LED, LOW);
   
   Serial.begin(115200);
   client.setInsecure();
@@ -56,6 +89,9 @@ void setup() {
   }
   Serial.println("\nConectado com sucesso ao broker!");
 
+mqtt.subscribe(topicIlum.c_str());
+
+mqtt.setCallback(callback);
 }
 
 long lerPresenca_1() {
@@ -86,6 +122,8 @@ long lerPresenca_2() {
 
 void loop() {
 
+  mqtt.subscribe(topicIlum.c_str());
+
   long presenca_1 = lerPresenca_1();
   long presenca_2 = lerPresenca_2();
   
@@ -95,6 +133,7 @@ void loop() {
 
   if (presenca_1 < 10) {
     Serial.println("Objeto próximo do primeiro sensor!");
+    mqtt.publish(topicPresença1.c_str(), "Esteve na estação 2");
   }
 
    Serial.println("---");
@@ -105,6 +144,7 @@ void loop() {
 
    if (presenca_2 < 10) {
     Serial.println("Objeto próximo do segundo sensor!");
+    mqtt.publish(topicPresença2.c_str(), "Esteve na estação 3");
   }
 
   Serial.println("---");
